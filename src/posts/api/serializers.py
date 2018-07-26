340e4067c31add42da7a9d3ed566a5e8e4a6d308
Anil <anilpython1@gmail.com>
2018-07-26 10:12:53 +0530

    Add some more classes

--- src/posts/api/serializers.py
+++ src/posts/api/serializers.py
@@ -1,10 +1,21 @@
 from rest_framework import serializers
 from posts.models import Post
 
+class PostCreateUpdateSerializer(serializers.ModelSerializer):
+	class Meta:
+		model = Post
+		fields = [
+		'title',
+		'slug',
+		'content',
+		'publish',
+		]
+
 class PostListSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Post
 		fields = [
+		'user',
 		'id',
 		'title',
 		'slug',
--- src/posts/api/views.py
+++ src/posts/api/views.py
@@ -3,10 +3,30 @@ from rest_framework.generics import (
 	RetrieveAPIView,
 	UpdateAPIView,
 	DestroyAPIView,
+	CreateAPIView,
 	)
 from posts.models import Post
-from .serializers import PostListSerializer, PostDetailSerializer
+from .serializers import (PostListSerializer, 
+	PostDetailSerializer, 
+	PostCreateUpdateSerializer,
+	)
 
+from rest_framework.permissions import (
+	AllowAny,
+	IsAuthenticated,
+	IsAdminUser,
+	IsAuthenticatedOrReadOnly,
+	)
+
+from .permissions import IsOwnerOrReadOnly
+
+class PostCreateAPIView(CreateAPIView):
+	queryset = Post.objects.all()
+	serializer_class = PostCreateUpdateSerializer
+	permission_classes = [IsAuthenticated,IsOwnerOrReadOnly]
+
+	def perform_create(self, serializer):
+		serializer.save(user=self.request.user) # title = "my title" we can pass field values here
 
 class PostDetailAPIView(RetrieveAPIView):
 	queryset = Post.objects.all()
@@ -15,8 +35,12 @@ class PostDetailAPIView(RetrieveAPIView):
 
 class PostUpdateAPIView(UpdateAPIView):
 	queryset = Post.objects.all()
-	serializer_class = PostDetailSerializer
+	serializer_class = PostCreateUpdateSerializer
 	lookup_field = "slug" # lookup_field_kwarg = "abc"
+	permission_classes = [IsAuthenticated, IsOwnerOrReadOnly]
+
+	def perform_update(self, serializer):
+		serializer.save(user=self.request.user)
 
 class PostDeleteAPIView(DestroyAPIView):
 	queryset = Post.objects.all()
